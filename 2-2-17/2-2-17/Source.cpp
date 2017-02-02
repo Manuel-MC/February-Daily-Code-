689e512854bbcbd6b821e931cac64d72761215b3
game110pm <game110pm@605-110-STU12>
2017-02-02 13:00:13 -0700

    jukebox code

--- 2-2-17/2-2-17/Source.cpp
+++ 2-2-17/2-2-17/Source.cpp
@@ -4,10 +4,19 @@ int main() {
 	int input;
 	cout << "Pick a number between 1-3" << endl;
 	cin >> input;
+
 	if (input == 1)
 		cout << "Kendrick Lamar-Backseat Freestyle" << endl;
-	else if (input == 2)
+	cout << "My mind is living on cloud nine and this nine is never on vacation" << endl;
+	cout << "Start up that Maserati and VROOM VROOM!I'm racing" << endl;
+
+	if (input == 2)
 		cout << "Eminem-Lose Yourself" << endl;
-	else if (input == 3)
+	cout << "Yo His palms are sweaty, knees weak, arms are heavy heres vomit on his sweater already, moms spaghetti" << endl;
+
+	if (input == 3)
 		cout << "Kid Cudi-Pursuit of Happiness" << endl;
+	cout << "I'm on the pursuit of happiness and I know" << endl;
+	cout << "Everything that's shine ain't always gonna be gold(hey)" << endl;
+	cout << "I'll be fine once I get it, get it in, I'll be good" << endl;
 }
