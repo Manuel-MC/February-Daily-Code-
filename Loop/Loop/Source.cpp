370eb652ad214928eaf77e5d2b68b1dd803ca006
game110pm <game110pm@605-110-STU12>
2017-02-16 14:49:32 -0700

    Moving Sqaures

---
+++ Loop/Loop/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 10; i < 25;i++)
+		cout << "L";
+	for (int o = 0;o < 5;o++)
+		cout << "O";
+	for (int l = 6;l < 10;l++)
+		cout << "L";
+
+}
